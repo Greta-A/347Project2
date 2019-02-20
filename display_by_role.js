93a0f4f54797a34f73a9fa31622df353e1a55c6a
Carl Clermont <theCarlClermont@gmail.com>
2019-02-20 15:31:26 -0500

    Added selector back in with it's event listener

--- display_by_role.js
+++ display_by_role.js
@@ -23,33 +23,33 @@ for (var possibleRoleVal in roleValues) {
 }
 
 
-// document.getElementById("role-selector")
-//     .addEventListener("change", function (event) {
-//         console.log("event", event);
-//         // loop over all possible roles (student, ta, teacher)
-//         for (var possibleRoleVal in roleValues) {
-//             // create the string classname for that role
-//             var currentClass = ".role-" + possibleRoleVal;
+document.getElementById("role-selector")
+    .addEventListener("change", function (event) {
+        console.log("event", event);
+        // loop over all possible roles (student, ta, teacher)
+        for (var possibleRoleVal in roleValues) {
+            // create the string classname for that role
+            var currentClass = ".role-" + possibleRoleVal;
 
-//             // select all of the elements that belong to the current role on the page
-//             var elements = document.querySelectorAll(currentClass);
-//             // debugger
-//             // if we have selected the currentRole of the loop in our dropdown
-//             // >= would allow teacher to see student elements, and etc.
-//             // if (classToRoleLevel(event.target.value) >= possibleRoleVal) { 
-//             if (classToRoleLevel(event.target.value) == possibleRoleVal) {
-//                 // add the class "showing" (and remove the class "hidden")
-//                 elements.forEach(elem => elem.classList.remove("hidden"))
-//                 elements.forEach(elem => elem.classList.add("showing"))
-//                 // elements.classList.add("showing")
-//             } else { //if we're looking at elements that do not belong to the sleected role, hide them
-//                 elements.forEach(elem => elem.classList.remove("showing"))
-//                 elements.forEach(elem => elem.classList.add("hidden"))
-//             }
-//         }
-//     });
+            // select all of the elements that belong to the current role on the page
+            var elements = document.querySelectorAll(currentClass);
+            // debugger
+            // if we have selected the currentRole of the loop in our dropdown
+            // >= would allow teacher to see student elements, and etc.
+            // if (classToRoleLevel(event.target.value) >= possibleRoleVal) { 
+            if (classToRoleLevel(event.target.value) == possibleRoleVal) {
+                // add the class "showing" (and remove the class "hidden")
+                elements.forEach(elem => elem.classList.remove("hidden"))
+                elements.forEach(elem => elem.classList.add("showing"))
+                // elements.classList.add("showing")
+            } else { //if we're looking at elements that do not belong to the sleected role, hide them
+                elements.forEach(elem => elem.classList.remove("showing"))
+                elements.forEach(elem => elem.classList.add("hidden"))
+            }
+        }
+    });
 
-// function classToRoleLevel (stringClass) {
-//     // "role-0" => 0
-//     return stringClass.split("-")[1]
-// }
+function classToRoleLevel (stringClass) {
+    // "role-0" => 0
+    return stringClass.split("-")[1]
+}
