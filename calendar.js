813420cc4f3fe762c1e28e9628d195eec8adf84d
Carl Clermont <theCarlClermont@gmail.com>
2019-02-27 13:54:53 -0500

    calendar add functionality

--- calendar.js
+++ calendar.js
@@ -32,48 +32,70 @@ function generateSessionCode()
 
 function addTATime()
 {
-// <button type="button" class="empty" style="display:none;"></button>
-// <button type="button" class="empty" style="display:none;"></button>
+  var addButton = document.getElementById("add");
+  if(addButton.textContent === "Add")
+  {
+    addButton.textContent = "Done";
+    //declarations
+    addButtons();
+  } else if (addButton.textContent === "Done")
+  {
+    addButton.textContent = "Add";
+    removeButtons();
+  }
+  else
+  {
+    console.log("Add button should have text of 'Add' or 'Done'");
+  }
+  
+}
 
-// <div class="role-1">
-//   <button type="button" class="empty" style="display:none;"></button>
-//   <button type="button" class="empty" style="display:none;"></button>
-// </div>
-
-  //get all tds in table.
-  var tds = $("td");
-  // 2 buttons per time.
+function addButtons()
+{
   var openSlots = 2; 
+  //get all tds in table.
+  var tds = document.getElementsByTagName("td");
+  //filter tds by ones that have div and ones that dont.
+  //if it has one add to it based on open slots.
+  //else create one and add two to it.
+  //var
   //Add buttons to div.
   for(var td of tds)
   {
-    // alert(td);
-    var timeDiv = $("<div><div>");//class='role-1'
-    // alert(timeDiv);
-    var slotButton = $("<button>+</button>");//type='button'//class='empty'
-    // alert(slotButton);
-    // openSlots = 2 - td.children().length;
-    // alert(openSlots);
-    for(var i = 0 ; i < openSlots ; i++)
+    var timeDiv = document.createElement("DIV");
+    timeDiv.setAttribute("class", "role-1");
+    var slotButton1 = document.createElement("BUTTON");
+    slotButton1.setAttribute("class", "empty");
+    slotButton1.setAttribute("type", "button");
+    //onclick="claimTime()"
+    var slotButton2 = document.createElement("BUTTON");
+    slotButton2.setAttribute("class", "empty");
+    slotButton2.setAttribute("type", "button");
+    //onclick="claimTime()"
+    if(openSlots === 2)
     {
-        timeDiv.append(slotButton);
+      timeDiv.appendChild(slotButton1);
     }
-    td.append(timeDiv);
-    td.append(slotButton);
+    if(openSlots >= 1) //|| openSLots === 2
+    {
+      timeDiv.appendChild(slotButton2);
+      td.appendChild(timeDiv);
+    }
   }
-
-
-//   var x = document.getElementsByClassName("empty");
-//   for (var i=0; i< x.length; i++)
-//   {
-//     if (x[i].style.display === "none")
-//     {
-//       x[i].style.display = "inline-block";
-//     }
-//     else
-//     {
-//       x[i].style.display = "none";
-//     }
-//   }
 }
 
+function removeButtons()
+{
+  var x = document.getElementsByClassName("empty");
+    for (var i=0; i< x.length; i++)
+    {
+      if (x[i].style.display === "none")
+      {
+//       x[i].style.display = "inline-block";
+      }
+      else
+      {
+        x[i].style.display = "none";
+      }
+    }
+}
