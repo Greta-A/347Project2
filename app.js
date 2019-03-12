79942cc7407cf112e093569a7842aae5600e647d
Greta-A <gretaalvesteffer@gmail.com>
2019-03-12 13:25:01 -0400

    loading html files using node

--- app.js
+++ app.js
@@ -4,9 +4,17 @@ var path    = require("path");
 
 app.use(express.static(__dirname + '/public'));
 
+// app.get('/',function(req,res){
+//   res.sendFile(path.join(__dirname+'/index.html'));
+// });
+
 app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
+  res.sendFile(path.join(__dirname+'/public/index.html'));
 });
+
+// var dir = path.join(__dirname, 'public');
+//
+// app.use(express.static(dir));
 
 app.listen(3000);
 
