de191ae12aaaff9421f77a1e02cab51bcf720b68
Greta-A <gretaalvesteffer@gmail.com>
2019-04-10 13:34:40 -0400

    backend questions

---
+++ public/js/questions_back.js
@@ -0,0 +1,14 @@
+var main = require('../../app.js');
+var index = require('./index.js');
+var courses = require('./course_list_back.js');
+var client = main.client;
+var pickedCourse = courses.pickedCourse;
+
+var methods = {
+  listenOnQuestions: function()
+  {
+
+  }
+}
+
+exports.data = methods;
