622808c6f8e05ae8d1e500e63ce57fa65d19f4e3
Carl Clermont <theCarlClermont@gmail.com>
2019-04-15 13:11:00 -0400

    start queue at :one:

--- public/js/questions.js
+++ public/js/questions.js
@@ -24,46 +24,49 @@ function displayQuestionList(jsonResponse)
   var sortedJSonQuestions = jsonResponse.sort(function(a,b){return a.position - b.position});
   for(jsonQuestion of sortedJSonQuestions)
   {
-    // access data by jsonQuestion.question or jsonQuestion.position, etc.
-    var surroundingDiv = document.createElement('div');
-    var question = document.createElement("li");
-    question.setAttribute("class", "question_item");
-    // set id of question to position in list for easy access
-    question.setAttribute("id", jsonQuestion.position);
-    question.innerHTML = jsonQuestion.owner + ": " + jsonQuestion.question;
-    surroundingDiv.appendChild(question);
-    // create form request for upvote button
-    var inputForm = document.createElement("form");
-    inputForm.setAttribute("action", "upvoteQuestion");
-    inputForm.setAttribute("method", "post");
-    var hiddenInput = document.createElement("input");
-    var hiddenInput2 = document.createElement("input");
-    hiddenInput.style.display = "none";
-    hiddenInput2.style.display = "none";
-    // need to create name to access number of upvotes
-    hiddenInput.setAttribute("name", "upvotes");
-    hiddenInput.value = jsonQuestion.upvotes;
-    hiddenInput2.setAttribute("name", "question");
-    hiddenInput2.value = jsonQuestion.question;
-    var upvoteBtn = document.createElement("button");
-    upvoteBtn.setAttribute('type', 'submit');
-    upvoteBtn.setAttribute('class', 'upvote');
-    upvoteBtn.innerHTML = "&#x1F44D; " + jsonQuestion.upvotes;
-    inputForm.appendChild(hiddenInput);
-    inputForm.appendChild(hiddenInput2);
-    inputForm.appendChild(upvoteBtn);
-    //question.appendChild(inputForm);
-    //surroundingDiv.appendChild(question);
-    surroundingDiv.appendChild(inputForm);
-    // create form for remove button
-    var removeBtn = document.createElement("button");
-    removeBtn.setAttribute('type', 'button');
-    removeBtn.setAttribute('class', 'remove');
-    removeBtn.innerHTML = "Remove";
-    //removeBtn.addEventListener("click", function(e) {removeQuestion(e)});
-    //surroundingDiv.appendChild(removeBtn);
-    var list = document.getElementById("questionList");
-    list.appendChild(surroundingDiv);
+    if(jsonQuestion.position != 0)
+    {
+      // access data by jsonQuestion.question or jsonQuestion.position, etc.
+      var surroundingDiv = document.createElement('div');
+      var question = document.createElement("li");
+      question.setAttribute("class", "question_item");
+      // set id of question to position in list for easy access
+      question.setAttribute("id", jsonQuestion.position);
+      question.innerHTML = jsonQuestion.owner + ": " + jsonQuestion.question;
+      surroundingDiv.appendChild(question);
+      // create form request for upvote button
+      var inputForm = document.createElement("form");
+      inputForm.setAttribute("action", "upvoteQuestion");
+      inputForm.setAttribute("method", "post");
+      var hiddenInput = document.createElement("input");
+      var hiddenInput2 = document.createElement("input");
+      hiddenInput.style.display = "none";
+      hiddenInput2.style.display = "none";
+      // need to create name to access number of upvotes
+      hiddenInput.setAttribute("name", "upvotes");
+      hiddenInput.value = jsonQuestion.upvotes;
+      hiddenInput2.setAttribute("name", "question");
+      hiddenInput2.value = jsonQuestion.question;
+      var upvoteBtn = document.createElement("button");
+      upvoteBtn.setAttribute('type', 'submit');
+      upvoteBtn.setAttribute('class', 'upvote');
+      upvoteBtn.innerHTML = "&#x1F44D; " + jsonQuestion.upvotes;
+      inputForm.appendChild(hiddenInput);
+      inputForm.appendChild(hiddenInput2);
+      inputForm.appendChild(upvoteBtn);
+      //question.appendChild(inputForm);
+      //surroundingDiv.appendChild(question);
+      surroundingDiv.appendChild(inputForm);
+      // create form for remove button
+      var removeBtn = document.createElement("button");
+      removeBtn.setAttribute('type', 'button');
+      removeBtn.setAttribute('class', 'remove');
+      removeBtn.innerHTML = "Remove";
+      //removeBtn.addEventListener("click", function(e) {removeQuestion(e)});
+      //surroundingDiv.appendChild(removeBtn);
+      var list = document.getElementById("questionList");
+      list.appendChild(surroundingDiv);
+    }
   }
 }
 
